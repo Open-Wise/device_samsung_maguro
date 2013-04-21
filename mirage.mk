4c477eeb150f9ea993cf9e18910b9dab07f17b14
franco-c <tjrdnjs33936@gmail.com>
2013-04-21 17:28:21 +0900

    MIRAGEFY
    Change-Id: I28a0c0bc5d7f115f3180e06b1d47a310cac10e35

--- mirage.mk
+++ mirage.mk
@@ -5,19 +5,19 @@ TARGET_BOOTANIMATION_NAME := vertical-720x1280
 $(call inherit-product, device/samsung/maguro/full_maguro.mk)
 
 # Inherit some common stuff.
-$(call inherit-product, vendor/slim/config/common_full_phone.mk)
+$(call inherit-product, vendor/mirage/config/common_full_phone.mk)
 
 # Inherit some common stuff.
-$(call inherit-product, vendor/slim/config/gsm.mk)
+$(call inherit-product, vendor/mirage/config/gsm.mk)
 
 # Inherit torch settings
-$(call inherit-product, vendor/slim/config/common_ledflash.mk)
+$(call inherit-product, vendor/mirage/config/common_ledflash.mk)
 
 # Inherit device settings
-$(call inherit-product, vendor/slim/config/common_nexus.mk)
+$(call inherit-product, vendor/mirage/config/common_nexus.mk)
 
 PRODUCT_COPY_FILES +=  \
-    vendor/slim/prebuilt/xhdpi/bootanimation.zip:system/media/bootanimation.zip
+    vendor/mirage/prebuilt/xhdpi/bootanimation.zip:system/media/bootanimation.zip
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.camera.res=5MP
@@ -28,7 +28,7 @@ PRODUCT_RELEASE_NAME := maguro
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=yakju BUILD_FINGERPRINT="google/yakju/maguro:4.2.2/JDQ39/573038:user/release-keys"
 PRIVATE_BUILD_DESC="yakju-user 4.2.2 JDQ39 573038 release-keys"
 
-PRODUCT_NAME := slim_maguro
+PRODUCT_NAME := mirage_maguro
 PRODUCT_DEVICE := maguro
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Galaxy Nexus
